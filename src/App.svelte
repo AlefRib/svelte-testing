fee68e1084ec744785d65fbea7caa517d6cd0e90
Alef Henrique Ribeiro <32271561+AlefRib@users.noreply.github.com>
2023-02-07 00:55:15 -0300

    Adiciona prettier, eslint e altera header

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,67 @@
 <script lang="ts">
-  import Dog1 from '../src/assets/images/dog1.png'
-  import Dog2 from '../src/assets/images/dog2.png'
-  import { writable } from 'svelte/store'
-  import logo from './assets/svelte.png'
-  import Card from './components/Card.svelte';
-  import PageTitle from './components/PageTitle.svelte'
+  import { Class } from "./../node_modules/@eslint/eslintrc/node_modules/type-fest/source/basic.d.ts";
+  import Dog1 from "../src/assets/images/dog1.png";
+  import Dog2 from "../src/assets/images/dog2.png";
+  import { writable } from "svelte/store";
+  import logo from "./assets/svelte.png";
+  import Card from "./components/Card.svelte";
+  import PageTitle from "./components/PageTitle.svelte";
 
-  const cardHome = writable([
-		{title:'Test1',cardTitle:'Title1',cardDescription:'description',image:Dog1, imageAlt:'Imagem teste',color:'bg-cyan-500',route:'/adotar'},
-    {title:'Test2',cardTitle:'Title2',cardDescription:'description2',image:Dog2, imageAlt:'Imag em teste',color:'bg-cyan-500',route:'/adotar1'}
-	])
+  const cardHome = [
+    {
+      title: "Test1",
+      cardTitle: "Title1",
+      cardDescription: "description",
+      image: Dog1,
+      imageAlt: "Imagem teste",
+      color: "bg-cyan-500",
+      route: "/adotar",
+    },
+    {
+      title: "Test2",
+      cardTitle: "Title2",
+      cardDescription: "description2",
+      image: Dog2,
+      imageAlt: "Imag em teste",
+      color: "bg-cyan-500",
+      route: "/adotar1",
+    },
+  ];
 </script>
 
 <main class="text-center mx-0">
-  <div class="bg-indigo-600 text-cyan-500 p-5 flex items-center justify-between" >
+  <div
+    class="bg-indigo-600 text-cyan-500 p-5 flex items-center justify-between"
+  >
+    <img class="w-10" alt="logo" src={logo} />
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24 w-5" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-      </svg>
-    </div>
-    <img class="w-10" alt="logo" src={logo}/>
-    <div>
-      <button class="max-w-xs sm:max-w-none my-4 text-lg font-semibold bg-cyan-500 text-indigo-600 p-3 rounded-full mr-4">
+      <button
+        class="max-w-xs sm:max-w-none my-4 text-lg font-semibold bg-cyan-500 text-indigo-600 p-3 rounded-full mr-4"
+      >
         Quer Ajudar?
       </button>
-      <button  class="max-w-xs sm:max-w-none my-4 text-lg font-semibold  ">
-       Entre em contato
+      <button class="max-w-xs sm:max-w-none my-4 text-lg font-semibold  ">
+        Entre em contato
       </button>
     </div>
   </div>
-  <PageTitle title="Quer Ajudar" subtitle="E você?" />
-  <div class="flex justify-center items-center gap-x-4">
-
-    {#each $cardHome as card}
+  <!-- <PageTitle title="Quer Ajudar" subtitle="E você?" /> -->
+  <div class="flex justify-center items-center gap-x-4 p-11">
+    <!-- {#each cardHome as card}
       <Card cardInfo={card}></Card>
-    {/each}	
+    {/each}	 -->
+
+    <div class="flex justify-between items-center  w-full">
+      <div class="text-start flex flex-col gap-8">
+        <h1 class="text-5xl">ONG DE ANIMAIS</h1>
+        <h3 class="text-3xl">
+          Ajudando a encontrar lares paras todos os animais
+        </h3>
+        <button class="bg-lime-700 p-6 text-white max-w-sm">Quero Adotar</button
+        >
+      </div>
+      <img alt="Imagem de um cachorro sentado" src={Dog1} />
+    </div>
   </div>
 </main>
 
@@ -42,9 +69,5 @@
   :root {
     --svelte-rgb: 255, 62, 0;
     background-color: rgb(79 70 229);
-  }
-
-  h1 {
-    color: rgb(var(--svelte-rgb));
   }
 </style>
--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,8 +1,16 @@
 <script>
-   export let cardInfo = {title:'',cardTitle:'',cardDescription:'',image:'', imageAlt:'',color:'',route:''};
+  export let cardInfo = {
+    title: "",
+    cardTitle: "",
+    cardDescription: "",
+    image: "",
+    imageAlt: "",
+    color: "",
+    route: "",
+  };
 </script>
 
-<div class={`w-56 h-80 rounded ${cardInfo.color}` }>
-    <img class='object-cover' alt={cardInfo.imageAlt} src={cardInfo.image}/>
-    <h2>{cardInfo.title}</h2>
+<div class={`w-56 h-80 rounded ${cardInfo.color}`}>
+  <img class="object-cover" alt={cardInfo.imageAlt} src={cardInfo.image} />
+  <h2>{cardInfo.title}</h2>
 </div>
